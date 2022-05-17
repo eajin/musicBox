1bdaae4a6935fe323e57e157df73dd5f0d6775c2
kjin <iyneed4573@gmail.com>
2022-05-17 18:40:55 +0900

    returnsTwo

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-const age = 96;
-function calculateKrage(ageOfForeigner) {
-  return ageOfForeigner + 2;
-}
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  mul: function (a, b) {
+    return a * b;
+  },
+  div: function (a, b) {
+    return a / b;
+  },
+  squared: function (a, b) {
+    return a ** b;
+  },
+};
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const mulResult = calculator.mul(minusResult, 10);
+const divResult = calculator.div(mulResult, 10);
+const squResult = calculator.squared(divResult, 10);
 
-const krage = calculateKrage(age);
-
-console.log(krage);
+console.log(plusResult);
+console.log(minusResult);
+console.log(mulResult);
+console.log(divResult);
+console.log(squResult);
