d6856f57fbc24a7aeb0667b80240d508914d742f
kjin <iyneed4573@gmail.com>
2022-05-17 16:41:01 +0900

    calculator

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-function plus(firstNumber, secondNumber) {
-  console.log(firstNumber + secondNumber);
-}
-
-plus(1, 2);
+const calculator = {
+  plus: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  mul: function (a, b) {
+    console.log(a * b);
+  },
+  div: function (a, b) {
+    console.log(a / b);
+  },
+  squared: function (a, b) {
+    console.log(a ** b);
+  },
+};
+calculator.plus(52, 34);
+calculator.minus(30, 50);
+calculator.mul(30, 20);
+calculator.div(34, 4);
+calculator.squared(6, 3);
