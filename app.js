36da45262bc06192689b0400437b825be6b81c76
kjin <iyneed4573@gmail.com>
2022-05-17 18:19:25 +0900

    Returns

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
-const calculator = {
-  plus: function (a, b) {
-    console.log(a + b);
-  },
-  minus: function (a, b) {
-    console.log(a - b);
-  },
-  mul: function (a, b) {
-    console.log(a * b);
-  },
-  div: function (a, b) {
-    console.log(a / b);
-  },
-  squared: function (a, b) {
-    console.log(a ** b);
-  },
-};
-calculator.plus(52, 34);
-calculator.minus(30, 50);
-calculator.mul(30, 20);
-calculator.div(34, 4);
-calculator.squared(6, 3);
+const age = 96;
+function calculateKrage(ageOfForeigner) {
+  return ageOfForeigner + 2;
+}
+
+const krage = calculateKrage(age);
+
+console.log(krage);
