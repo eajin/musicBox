28043fdfb2d7ae584b8ba89f5389cf87e52ca696
kjin <iyneed4573@gmail.com>
2022-05-23 15:26:25 +0900

    toggle

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const h1 = document.querySelector("div.hello h1");
 
 function handleClick() {
-  const clickedClass = "active";
-  if (h1.classList.contains(clickedClass)) {
-    h1.classList.remove(clickedClass);
-  } else {
-    h1.classList.add(clickedClass);
-  }
+  h1.classList.toggle("active");
 }
 
 h1.addEventListener("click", handleClick);
