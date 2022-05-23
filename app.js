fd065e71bab70e10def4fdf3386b66891cec107e
kjin <iyneed4573@gmail.com>
2022-05-23 15:23:06 +0900

    classList

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const h1 = document.querySelector("div.hello h1");
 
 function handleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
+  const clickedClass = "active";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
   } else {
-    newColor = "blue";
+    h1.classList.add(clickedClass);
   }
-  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleClick);
